c33fe018f52477e1cfb149a0f1c954b743fd224a
Darya Tarelko <darya.tarelko@sephora.com>
2020-10-15 10:31:24 +0300

    Improvements: WebServicesTests implemented

---
+++ year_2020/src/test/java/com/epam/atm/yandex/webservices/WebServicesTests.java
@@ -0,0 +1,42 @@
+package com.epam.atm.yandex.webservices;
+
+import com.epam.atm.yandex.reporting.Logger;
+import io.restassured.RestAssured;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.junit.Assert;
+import static io.restassured.RestAssured.given;
+import org.json.simple.parser.JSONParser;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WebServicesTests {
+
+	@Before
+	public void initTest() {
+		RestAssured.baseURI = "https://www.sephora.com";
+	}
+ 	@Test
+	public void checkStatusCode() {
+		int statusCode = given().get("/api/users/profiles/current/full").andReturn().getStatusCode();
+		Logger.info(String.format("statusCode ACTUAL VALUE:   - %1$s", statusCode));
+		Logger.info(String.format("statusCode EXPECTED VALUE: - %1$s", 200));
+		Assert.assertEquals(statusCode, 200);
+	}
+	@Test
+	public void checkResponseBody() {
+		String responseBody = given().get("/api/users/profiles/current/full").getBody().asString();
+		try {
+			JSONObject jsonData = (JSONObject) new JSONParser().parse(responseBody);
+			JSONObject basket = (JSONObject) jsonData.get("basket");
+			String maxSamplesAllowedPerOrder = basket.get("maxSamplesAllowedPerOrder").toString();
+			String expectedMaxSamplesAllowedPerOrder = "2";
+			Logger.info(String.format("maxSamplesAllowedPerOrder ACTUAL VALUE:   - %1$s", maxSamplesAllowedPerOrder));
+			Logger.info(String.format("maxSamplesAllowedPerOrder EXPECTED VALUE: - %1$s", expectedMaxSamplesAllowedPerOrder));
+			Assert.assertEquals(maxSamplesAllowedPerOrder,expectedMaxSamplesAllowedPerOrder);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+}
